c2d7fcab3de6705fe140829497093e517fb353fd
Shivani Thakur <shivthakuur09@gmail.com>
2023-06-29 11:06:40 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,49 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={{float:"left",color:"blue", marginLeft:"1%"}}><a href="https://www.flipkart.com" target="_blank">Flipkart</a></h1>
+      <input type="search" placeholder='Search'  style={{textAlign:"left",marginTop:"5%", marginLeft:"-8%"}}/>
+       <br /><br />
+       <div>
+      <a href='' style={{marginLeft:"9px"}}>Become a seller</a> <br />
+
+       </div>
+       <br /> <br />
+       <div>
+              <a href='' style={{marginLeft:"8px"}}>Cart</a> <br />
+
+       </div>
+<br />
+       <div style={{border:"1px solid grey"}}></div>
+     <img loading="eager" class="_396cs4" alt="APPLE iPhone 13 (Pink, 128 GB)" style={{marginLeft:"1%"}}src="https://rukminim1.flixcart.com/image/312/312/ktketu80/mobile/8/z/w/iphone-13-mlph3hn-a-apple-original-imag6vzzhrxgazsg.jpeg?q=70"></img>
+    <div>
+    <a href="" style={{marginTop:"10%", marginLeft:"1%"}} >APPLE iPhone 11 (White, 128 GB)</a>
+
     </div>
+    
+    <div style={{marginLeft:"1%"}}>
+      <b>₹ 44,999   <del>₹48,999</del> 7% off  free delivery</b>
+      <br />
+      <i>1,81,870 Rating & 10,731 Reviews</i>
+    </div>
+
+    <div style={{marginLeft:"1%"}}>
+      Upto <b>₹20,000</b> off an Exchange
+        <li style={{marginLeft:"3%"}}>128 GB ROM</li>
+        <li style={{marginLeft:"3%"}}>15.49 cm (6.1 inch) Liquid Retina HD Display</li>
+        <li style={{marginLeft:"3%"}}>12MP + 12MP | 12MP Front Camera</li>
+        <li style={{marginLeft:"3%"}}>A 13 Bionic Chip Processor </li>
+    </div>
+    <br />
+
+    <div style={{border:"1px solid grey"}}></div>
+    <div>
+    <a href="" style={{marginTop:"10%"}}>About</a> &nbsp;&nbsp; <a href="" style={{marginTop:"10%"}}>FAQs</a> &nbsp;&nbsp; <a href="" style={{marginTop:"10%"}}>Privacy</a>  <a href="" style={{marginTop:"10%"}}>Policy</a>
+
+    </div>
+
+      </div>
   );
 }
 
